413f909b99cfe0576ae01666c3a2356147fd5400
fo95116 <fo95116@gmail.com>
2016-08-21 18:19:35 -0700

    add member function for block

--- wordpress/wp-content/themes/theme/functions.php
+++ wordpress/wp-content/themes/theme/functions.php
@@ -115,6 +115,39 @@ function theme_scripts() {
 add_action( 'wp_enqueue_scripts', 'theme_scripts' );
 
 /**
+ * Register `team` post type
+ */
+function team_post_type() {
+
+   // Labels
+	$labels = array(
+		'name' => _x("Team", "post type general name"),
+		'singular_name' => _x("Team", "post type singular name"),
+		'menu_name' => 'Team Profiles',
+		'add_new' => _x("Add New", "team item"),
+		'add_new_item' => __("Add New Profile"),
+		'edit_item' => __("Edit Profile"),
+		'new_item' => __("New Profile"),
+		'view_item' => __("View Profile"),
+		'search_items' => __("Search Profiles"),
+		'not_found' =>  __("No Profiles Found"),
+		'not_found_in_trash' => __("No Profiles Found in Trash"),
+		'parent_item_colon' => ''
+	);
+
+	// Register post type
+	register_post_type('team' , array(
+		'labels' => $labels,
+		'public' => true,
+		'has_archive' => false,
+		'menu_icon' => get_stylesheet_directory_uri() . '/lib/TeamProfiles/team-icon.png',
+		'rewrite' => false,
+		'supports' => array('title', 'editor', 'thumbnail')
+	) );
+}
+add_action( 'init', 'team_post_type', 0 );
+
+/**
  * Implement the Custom Header feature.
  */
 require get_template_directory() . '/inc/custom-header.php';
